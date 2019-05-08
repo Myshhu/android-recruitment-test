d456efaa19380281f27cb68c261f46163d5b905b
Myshhu <kuba1260@gmail.com>
2019-05-08 22:05:58 +0200

    Add simple database and ability to save and read items from it

---
+++ app/src/main/java/dog/snow/androidrecruittest/DBHelper.java
@@ -0,0 +1,86 @@
+package dog.snow.androidrecruittest;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.os.Build.CPU_ABI;
+import static android.os.Build.ID;
+import static java.sql.Types.DOUBLE;
+import static java.sql.Types.INTEGER;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DBHelper";
+
+    private static final String TABLE_NAME = "JSON_Items";
+    private static final String COLUMN1 = "id";
+    private static final String COLUMN2 = "name";
+    private static final String COLUMN3 = "description";
+    private static final String COLUMN4 = "icon";
+    private static final String COLUMN5 = "timestamp";
+    private static final String COLUMN6 = "url";
+
+    DBHelper(Context context) {
+        super(context, TABLE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + "(" + COLUMN1 + " INTEGER, " +
+                  COLUMN2 + " TEXT, " + COLUMN3 + " TEXT, " + COLUMN4 + " TEXT, " + COLUMN5 + " LONG, " + COLUMN6 + " TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        /*sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);*/
+    }
+
+    public boolean addItem(JSONObject object) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        try {
+            contentValues.put(COLUMN1, object.getInt(COLUMN1));
+            contentValues.put(COLUMN2, object.getString(COLUMN2));
+            contentValues.put(COLUMN3, object.getString(COLUMN3));
+            contentValues.put(COLUMN4, object.getString(COLUMN4));
+            contentValues.put(COLUMN5, object.getLong(COLUMN5));
+            contentValues.put(COLUMN6, object.getString(COLUMN6));
+
+            long result = database.insert(TABLE_NAME, null, contentValues);
+
+            return (result != -1); //Return false when inserting failed
+        } catch (JSONException e) {
+            Log.e(TAG, "Adding to database failed", e);
+            return false;
+        }
+    }
+
+    boolean addArray(JSONArray array) {
+        boolean result = true;
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                JSONObject object = array.getJSONObject(i);
+                result = addItem(object);
+            } catch (Exception e) {
+                Log.e(TAG, "addArray function failed", e);
+            }
+        }
+        return result; //Return true if all objects were successfully added
+    }
+
+    Cursor getItems() {
+        SQLiteDatabase database = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+        return database.rawQuery(query, null);
+    }
+}
---
+++ app/src/main/java/dog/snow/androidrecruittest/DataDownloader.java
@@ -0,0 +1,78 @@
+package dog.snow.androidrecruittest;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DataDownloader extends AsyncTask <Void, Void, Void> {
+
+    private static final String SERVER_URL = "http://1bbdf2f3.ngrok.io/api/items";
+    private static final String TAG = MainActivity.class.getName();
+    private JSONArray downloadedJSONArray;
+    private final WeakReference<Context> weakContext; //Avoid memory leak
+    private DBHelper dbHelper;
+
+    DataDownloader(Context context) {
+        this.weakContext = new WeakReference<>(context);
+        downloadedJSONArray = new JSONArray();
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL(SERVER_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            InputStream inputStream = connection.getInputStream();
+            downloadedJSONArray = createJSONArrayFromInputStream(inputStream);
+        } catch (Exception e) {
+            Log.e(TAG, "Data downloading error", e);
+        }
+
+        Log.d(TAG, downloadedJSONArray.toString());
+
+        dbHelper = new DBHelper(weakContext.get());
+        dbHelper.addArray(downloadedJSONArray);
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        Cursor items = dbHelper.getItems();
+        while (items.moveToNext()) {
+            String result = items.getInt(0) + " " +
+                    items.getString(1) + " " +
+                    items.getString(2) + " " +
+                    items.getString(3) + " " +
+                    items.getLong(4) + " " +
+                    items.getString(5) + " ";
+                    Log.i(TAG, result);
+        }
+    }
+
+    private JSONArray createJSONArrayFromInputStream(InputStream inputStream) throws IOException, JSONException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder result = new StringBuilder();
+        String line;
+        while((line = bufferedReader.readLine()) != null) {
+            result.append(line).append("\n");
+        }
+        return new JSONArray(result.toString());
+    }
+}
